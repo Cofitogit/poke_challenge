f63d611ff1c3a02721e8b74f1802415a078911b4
cofitogit <kikelin32@gmail.com>
2025-04-16 11:08:46 -0300

    fix(battle): better ui handling

--- frontend/lib/features/battle/presentation/pages/pokemon_selection_page.dart
+++ frontend/lib/features/battle/presentation/pages/pokemon_selection_page.dart
@@ -47,154 +47,157 @@ class _PokemonSelectionPageState extends State<PokemonSelectionPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: ConstrainedBox(
-            constraints: const BoxConstraints(maxWidth: 900),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                const SizedBox(height: 16.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Battle of Pokemon',
-                      style: Theme.of(context).textTheme.titleLarge?.copyWith(fontSize: 32),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 24),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Select your pokemon',
-                      style: Theme.of(context).textTheme.headlineSmall,
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 16),
-                Flexible(
-                  flex: 2,
-                  child: Consumer<PokemonProvider>(
-                    builder: (context, provider, child) {
-                      if (provider.state == PokemonLoadState.loading) {
-                        return const Center(child: CircularProgressIndicator());
-                      } else if (provider.state == PokemonLoadState.error) {
-                        return Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'Error loading pokemon list',
-                                style: Theme.of(context).textTheme.titleLarge,
-                              ),
-                              const SizedBox(height: 8),
-                              Text(provider.errorMessage),
-                              const SizedBox(height: 16),
-                              ElevatedButton(
-                                onPressed: () => provider.loadPokemons(),
-                                child: const Text('Retry'),
-                              ),
-                            ],
-                          ),
-                        );
-                      } else if (provider.pokemons.isEmpty) {
-                        return const Center(
-                          child: Text('No pokemons available'),
-                        );
-                      }
-      
-                      return LayoutBuilder(builder: (context, constraints) {
-                        final crossAxisCount = _getCrossAxisCount(constraints.maxWidth);
-      
-                        return GridView.builder(
-                          padding: const EdgeInsets.all(16),
-                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: crossAxisCount,
-                            crossAxisSpacing: 16,
-                            mainAxisSpacing: 16,
-                            childAspectRatio: 1,
-                          ),
-                          itemCount: provider.pokemons.length,
-                          shrinkWrap: true,
-                          itemBuilder: (context, index) {
-                            final pokemon = provider.pokemons[index];
-                            return PokemonCard(
-                              pokemon: pokemon,
-                              isSelected: _selectedPokemon?.id == pokemon.id,
-                              onTap: () {
-                                setState(() {
-                                  _selectedPokemon = pokemon;
-                                  _resetBattle();
-                                });
-                              },
-                            );
-                          },
-                        );
-                      });
-                    },
+        child: SingleChildScrollView(
+          child: Center(
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 900),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const SizedBox(height: 16.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Battle of Pokemon',
+                        style: Theme.of(context).textTheme.titleLarge?.copyWith(fontSize: 32),
+                      ),
+                    ],
                   ),
-                ),
-                // Área de la batalla
-                if (_selectedPokemon != null)
-                  Expanded(
-                    flex: 3,
-                    child: Consumer<BattleProvider>(
-                      builder: (context, battleProvider, child) {
-                        if (battleProvider.state == BattleState.completed && battleProvider.battleResult != null) {
-                          return Column(
-                            children: [
-                              TweenAnimationBuilder(
-                                duration: const Duration(milliseconds: 300),
-                                tween: Tween<double>(begin: 0, end: 1),
-                                builder: (context, value, child) => ConstrainedBox(
-                                  constraints: BoxConstraints(maxHeight: value * 400),
-                                  child: Opacity(
-                                    opacity: value,
-                                    child: Container(
-                                      width: double.infinity,
-                                      margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-                                      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
-                                      decoration: BoxDecoration(
-                                        color: const Color(0XFFe4f8fe),
-                                        borderRadius: BorderRadius.circular(4.0),
-                                        border: Border.all(color: Colors.black, width: 1.0),
-                                        boxShadow: const [
-                                          BoxShadow(
-                                            color: Color.fromARGB(255, 146, 146, 146),
-                                            offset: Offset(0, 3),
-                                            blurRadius: 6,
-                                            spreadRadius: 2,
-                                          ),
-                                        ],
-                                      ),
-                                      child: Row(
-                                        mainAxisAlignment: MainAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            '${battleProvider.battleResult?.winner.name} wins!',
-                                            style: Theme.of(context).textTheme.titleLarge,
-                                          ),
-                                        ],
+                  const SizedBox(height: 24),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Select your pokemon',
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  Flexible(
+                    flex: 2,
+                    child: Consumer<PokemonProvider>(
+                      builder: (context, provider, child) {
+                        if (provider.state == PokemonLoadState.loading) {
+                          return const Center(child: CircularProgressIndicator());
+                        } else if (provider.state == PokemonLoadState.error) {
+                          return Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'Error loading pokemon list',
+                                  style: Theme.of(context).textTheme.titleLarge,
+                                ),
+                                const SizedBox(height: 8),
+                                Text(provider.errorMessage),
+                                const SizedBox(height: 16),
+                                ElevatedButton(
+                                  onPressed: () => provider.loadPokemons(),
+                                  child: const Text('Retry'),
+                                ),
+                              ],
+                            ),
+                          );
+                        } else if (provider.pokemons.isEmpty) {
+                          return const Center(
+                            child: Text('No pokemons available'),
+                          );
+                        }
+                    
+                        return LayoutBuilder(builder: (context, constraints) {
+                          final crossAxisCount = _getCrossAxisCount(constraints.maxWidth);
+                    
+                          return GridView.builder(
+                            padding: const EdgeInsets.all(16),
+                            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: crossAxisCount,
+                              crossAxisSpacing: 16,
+                              mainAxisSpacing: 16,
+                              childAspectRatio: 1,
+                            ),
+                            itemCount: provider.pokemons.length,
+                            shrinkWrap: true,
+                            itemBuilder: (context, index) {
+                              final pokemon = provider.pokemons[index];
+                              return PokemonCard(
+                                pokemon: pokemon,
+                                isSelected: _selectedPokemon?.id == pokemon.id,
+                                onTap: () {
+                                  setState(() {
+                                    _selectedPokemon = pokemon;
+                                    _resetBattle();
+                                  });
+                                },
+                              );
+                            },
+                          );
+                        });
+                      },
+                    ),
+                  ),
+                  // Área de la batalla
+                  if (_selectedPokemon != null)
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.6,
+                      child: Consumer<BattleProvider>(
+                        builder: (context, battleProvider, child) {
+                          if (battleProvider.state == BattleState.completed && battleProvider.battleResult != null) {
+                            return Column(
+                              children: [
+                                TweenAnimationBuilder(
+                                  duration: const Duration(milliseconds: 300),
+                                  tween: Tween<double>(begin: 0, end: 1),
+                                  builder: (context, value, child) => ConstrainedBox(
+                                    constraints: BoxConstraints(maxHeight: value * 400),
+                                    child: Opacity(
+                                      opacity: value,
+                                      child: Container(
+                                        width: double.infinity,
+                                        margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                                        padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+                                        decoration: BoxDecoration(
+                                          color: const Color(0XFFe4f8fe),
+                                          borderRadius: BorderRadius.circular(4.0),
+                                          border: Border.all(color: Colors.black, width: 1.0),
+                                          boxShadow: const [
+                                            BoxShadow(
+                                              color: Color.fromARGB(255, 146, 146, 146),
+                                              offset: Offset(0, 3),
+                                              blurRadius: 6,
+                                              spreadRadius: 2,
+                                            ),
+                                          ],
+                                        ),
+                                        child: Row(
+                                          mainAxisAlignment: MainAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              '${battleProvider.battleResult?.winner.name} wins!',
+                                              style: Theme.of(context).textTheme.titleLarge,
+                                            ),
+                                          ],
+                                        ),
                                       ),
                                     ),
                                   ),
                                 ),
-                              ),
-                              Expanded(
-                                child: _buildBattleArea(battleProvider),
-                              ),
-                            ],
-                          );
-                        }
-                        
-                        return _buildBattleArea(battleProvider);
-                      },
+                                Expanded(
+                                  child: _buildBattleArea(battleProvider),
+                                ),
+                              ],
+                            );
+                          }
+                          
+                          return _buildBattleArea(battleProvider);
+                        },
+                      ),
                     ),
-                  ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
