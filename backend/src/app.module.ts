b91784bac7217a705ab7f41d6d66346627ff2f4b
cofitogit <kikelin32@gmail.com>
2025-04-16 01:37:56 -0300

    feat(app): configure TypeORM with SQLite and add Pokemon module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'pokemon.db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, //! remove when finished
+    }),
+    PokemonModule,
+  ],
 })
 export class AppModule {}
